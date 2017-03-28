00a33cc3156fa5710db93bb382884988f47ae2db
Aeolitus <praiodor@gmail.com>
2017-03-29 00:10:43 +0200

    Adding pdftk for easier maintenance and since its in the script now.

--- build.bat
+++ build.bat
@@ -6,3 +6,4 @@ COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\IncludeInBuildFolder\Library\plugins
 C:\Users\Aeolitus\Desktop\Sephrasto\Build\Library\plugins\platforms
 COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\datenbank.xml C:\Users\Aeolitus\Desktop\Sephrasto\Build
 COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\Charakterbogen.pdf C:\Users\Aeolitus\Desktop\Sephrasto\Build
+COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\pdftk_server-2.02-win-setup.exe C:\Users\Aeolitus\Desktop\Sephrasto\Build
