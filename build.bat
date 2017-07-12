7b6de1da459734e31a8b6bc8cf70fe6fe10451fb
Aeolitus <praiodor@gmail.com>
2017-07-12 22:37:39 +0200

    Removed Setup Exe from build.bat

--- build.bat
+++ build.bat
@@ -8,4 +8,3 @@ COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\IncludeInBuildFolder\Library\plugins
 C:\Users\Aeolitus\Desktop\Sephrasto\Build\Library\plugins\platforms
 COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\datenbank.xml C:\Users\Aeolitus\Desktop\Sephrasto\Build
 COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\Charakterbogen.pdf C:\Users\Aeolitus\Desktop\Sephrasto\Build
-COPY /Y C:\Users\Aeolitus\Desktop\Sephrasto\pdftk_server-2.02-win-setup.exe C:\Users\Aeolitus\Desktop\Sephrasto\Build
