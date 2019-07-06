d0539f4384c21d2c3fdc88ded4ed30b114ffb52f
brzGatsu <worszeck@googlemail.com>
2019-07-06 22:02:34 +0200

    Weapon picker: Add search bar. Fixed current weapon not getting selected.

--- WaffenPicker.py
+++ WaffenPicker.py
@@ -31,37 +31,14 @@ class WaffenPicker(object):
                 QtCore.Qt.WindowCloseButtonHint)
         
         logging.debug("Ui is Setup...")
-        currSet = self.current != ""
-
-        kampfferts = self.findKampfFertigkeiten()
-
-        for kind in kampfferts:
-            parent = QtWidgets.QTreeWidgetItem(self.ui.treeWeapons)
-            parent.setText(0,kind.name)
-            parent.setText(1,"")
-            parent.setExpanded(True)
-            wafs = []
-            for waf in Wolke.DB.waffen:
-                if Wolke.DB.waffen[waf].fertigkeit == kind.name:
-                    wafs.append(waf)
-            wafs.sort()
-            for el in wafs:
-                if not currSet:
-                    self.current = el
-                    currSet = True
-                child = QtWidgets.QTreeWidgetItem(parent)
-                child.setText(0,Wolke.DB.waffen[el].anzeigename or el)
-                child.setText(1,Wolke.DB.waffen[el].talent)
-                child.setData(0, QtCore.Qt.UserRole, el) # store key of weapon in user data
-        self.ui.treeWeapons.sortItems(1,QtCore.Qt.AscendingOrder)
+        self.populateTree()
         logging.debug("Tree Filled...")
         self.ui.treeWeapons.itemSelectionChanged.connect(self.changeHandler)
         self.ui.treeWeapons.header().setSectionResizeMode(0,1)
         self.ui.treeWeapons.setFocus()
-        self.ui.treeWeapons.setCurrentItem(self.ui.treeWeapons.topLevelItem(0).child(0), 0, QtCore.QItemSelectionModel.Select)
         self.updateInfo()
         logging.debug("Info Updated...")
-
+        self.ui.nameFilterEdit.textChanged.connect(self.populateTree)
         self.Form.setWindowModality(QtCore.Qt.ApplicationModal)
         self.Form.show()
         self.ret = self.Form.exec_()
@@ -70,6 +47,45 @@ class WaffenPicker(object):
         else:
             self.waffe = None
 
+    def populateTree(self):
+        currSet = self.current != ""
+        self.ui.treeWeapons.clear();
+        kampfferts = self.findKampfFertigkeiten()
+        for kind in kampfferts:
+            wafs = []
+            for waf in Wolke.DB.waffen:
+                if self.ui.nameFilterEdit.text() and not self.ui.nameFilterEdit.text().lower() in Wolke.DB.waffen[waf].anzeigename.lower():
+                    continue
+                if Wolke.DB.waffen[waf].fertigkeit == kind.name:
+                    wafs.append(waf)
+            wafs.sort()
+            if len(wafs) == 0:
+                continue
+
+            parent = QtWidgets.QTreeWidgetItem(self.ui.treeWeapons)
+            parent.setText(0,kind.name)
+            parent.setText(1,"")
+            parent.setExpanded(True)
+            for el in wafs:
+                if not currSet:
+                    self.current = el
+                    currSet = True
+                child = QtWidgets.QTreeWidgetItem(parent)
+                child.setText(0,Wolke.DB.waffen[el].anzeigename or el)
+                child.setText(1,Wolke.DB.waffen[el].talent)
+                child.setData(0, QtCore.Qt.UserRole, el) # store key of weapon in user data
+
+        self.ui.treeWeapons.sortItems(1,QtCore.Qt.AscendingOrder)
+
+        if self.current in Wolke.DB.waffen:
+            found = self.ui.treeWeapons.findItems(Wolke.DB.waffen[self.current].anzeigename, QtCore.Qt.MatchExactly | QtCore.Qt.MatchRecursive)
+            if len(found) > 0:
+                self.ui.treeWeapons.setCurrentItem(found[0], 0, QtCore.QItemSelectionModel.Select)
+        elif self.ui.treeWeapons.topLevelItemCount() > 0 and self.ui.treeWeapons.topLevelItem(0).childCount() > 0:
+            self.ui.treeWeapons.setCurrentItem(self.ui.treeWeapons.topLevelItem(0).child(0), 0, QtCore.QItemSelectionModel.Select)
+        self.changeHandler()
+
+
     def findKampfFertigkeiten(self):
         return [el for el in Wolke.DB.fertigkeiten.values() if el.kampffertigkeit > 0]
 
@@ -77,7 +93,7 @@ class WaffenPicker(object):
         kampfferts = []
         for fert in self.findKampfFertigkeiten():
             kampfferts.append(fert.name)
-
+        self.current = ""
         for el in self.ui.treeWeapons.selectedItems():
             if el.text(0) in kampfferts:
                 continue
@@ -86,7 +102,19 @@ class WaffenPicker(object):
         self.updateInfo()
         
     def updateInfo(self):
-        if self.current != "":
+        self.ui.buttonBox.buttons()[0].setEnabled(self.current != "")
+        if self.current == "":
+            self.ui.labelName.setText("Keine Waffe selektiert")
+            self.ui.labelTyp.setText("")
+            self.ui.labelFert.setText("")
+            self.ui.plainStile.setPlainText("")
+            self.ui.labelTP.setText("")
+            self.ui.labelRW.setText("")
+            self.ui.labelWMLZ_Text.setText("Waffenmodifikator")
+            self.ui.labelWMLZ.setText("")
+            self.ui.labelH.setText("")
+            self.ui.plainEigenschaften.setPlainText("")
+        else:
             w = Wolke.DB.waffen[self.current]
             self.ui.labelName.setText(w.name)
             if type(w) == Objekte.Nahkampfwaffe:
