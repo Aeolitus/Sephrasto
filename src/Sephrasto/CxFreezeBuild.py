58c2e17bd782ac2760589c608941a28c7ab2a2ce
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2023-08-15 11:00:01 +0200

    Update cx_Freeze to 6.15.5 Update buildscript to remove a lot of unneeded packages and to include shiboken into the zipfile (possible since latest cx_Freeze update)

--- src/Sephrasto/CxFreezeBuild.py
+++ src/Sephrasto/CxFreezeBuild.py
@@ -16,6 +16,8 @@ doc_path = os.path.join(build_path, "Doc")
 bin_path = os.path.join(build_path, "Bin")
 data_path = os.path.join(build_path, "Data")
 
+system = platform.system()
+
 # Clean buildfolder and create build root folder
 def cleanBuildFolder():
     for filename in os.listdir(build_path_root):
@@ -35,16 +37,17 @@ else:
 print("Running cxfreeze")
 build_exe_options = {
     "build_exe" : build_path,
+    "bin_excludes": ["Qt6Pdf.dll", "Qt6PdfWidgets.dll", "Qt6PdfQuick.dll", "Qt6Svg.dll", "Qt6VirtualKeyboard.dll", "Qt6WebSockets.dll", "Qt6WebEngineQuick.dll"],
     "includes" : ["multiprocessing", "PySide6.QtPrintSupport", "PySide6.QtWebChannel", "PySide6.QtWebEngineWidgets", "QtUtils.SimpleSettingsDialog"],
-    "excludes" : ["tkinter", "distutils", "html", "unittest", "pydoc", "bz2", "pyexpat", "lzma"],
+    "excludes" : ["tkinter", "distutils", "html", "unittest", "pydoc", "bz2", "pyexpat", "lzma", "email", "xml", "ssl", "socket", "_hashlib", "http", "test", "asyncio", "queue", "select", "xmlrpc", "PySide6.QtSvg", "PySide6.QtVirtualKeyboard", "PySide6.QtWebSockets", "PySide6.QtWebEngineQuick"],
     "optimize" : 2,
     "zip_include_packages" : "*",
-    "zip_exclude_packages" : ["shiboken6"],
-    "include_msvcr" : True
+    "zip_exclude_packages" : [],
+    "include_msvcr" : True,
+    "include_files" : ["Data", "Doc", "icon_large.png", "icon_multi.ico", ["Bin/" + system, "Bin/" + system]]
 }
 
 base = None
-system = platform.system()
 if system == "Windows":
     base = "Win32GUI"
 
@@ -59,17 +62,22 @@ setup(  name = 'Sephrasto',
 print("Removing unneeded files")
 removeFiles = [
     "frozen_application_license.txt", #we already have that in Acknowledgements.md
-    "lib/PySide6/Qt6Svg.dll",
-    "lib/PySide6/Qt6Pdf.dll",
     "lib/PySide6/opengl32sw.dll",
+    "lib/PySide6/QtWebSockets.pyd",
+    "lib/PySide6/QtSvg.pyd",
+    "lib/PySide6/QtWebEngineQuick.pyd",
     "lib/PySide6/resources/qtwebengine_devtools_resources.pak",
     "lib/PySide6/resources/qtwebengine_resources_100p.pak",
     "lib/PySide6/resources/qtwebengine_resources_200p.pak",
     "lib/PySide6/translations",
     "lib/PySide6/bin",
+    "lib/PySide6/plugins/tls",
+    "lib/PySide6/plugins/platforminputcontexts",
+    "lib/PySide6/plugins/networkinformation",
+    "lib/PySide6/plugins/iconengines",
+    "lib/PySide6/plugins/generic",
+    "lib/PySide6/plugins/imageformats",
     "lib/PySide6/d3dcompiler_47.dll",
-    "lib/libcrypto-1_1.dll",
-    "lib/libssl-1_1.dll",
     "lib/D3DCOMPILER_47.dll",
 ]
 
@@ -82,36 +90,6 @@ for filename in removeFiles:
         except OSError:
             os.remove(filepath)
              
-# Copy additional files and folders to build folder
-print("Copying additional files and folders to build folder")
-
-includeFiles = {
-    "Data" : data_path,
-    "Doc" : doc_path,
-    "icon_large.png" : build_path,
-    "icon_multi.ico" : build_path,
-}
-
-includeFiles.update({
-    "Bin/" + system : os.path.join(bin_path, system)
-})
-
-# Now do the actual copying
-for file,targetDir in includeFiles.items():
-    if not os.path.isfile(file) and not os.path.isdir(file):
-        print("Error, file not found: " + file)
-        cleanBuildFolder()
-        sys.exit()
-
-    if os.path.isfile(file):
-        if not os.path.exists(targetDir):
-            os.makedirs(targetDir)
-        shutil.copy2(file, targetDir)
-    else:
-        if not os.path.exists(os.path.dirname(targetDir)):
-            os.makedirs(os.path.dirname(targetDir))
-        shutil.copytree(file, targetDir)
-
 # Zip the build
 archiveName = "Sephrasto_v" + str(Version._sephrasto_version_major) + "." + str(Version._sephrasto_version_minor) + "." + str(Version._sephrasto_version_build)
 print("Creating archive " + archiveName + ".zip")
@@ -120,6 +98,7 @@ print("Creating archive " + archiveName + ".zip")
 if system == "Windows":
     # We are using 7-zip for better compression with deflate64. This compression is natively supported by windows 7+.
     # This is a nice tool for building the commandline args: https://axelstudios.github.io/7z/#!/
+    # Note: Windows 11 now natively supports .7z, sometime in the future we can maybe switch...
     sevenZip = os.path.join(project_path, "tools", "7-zip", "7za.exe")
     subprocess.check_output([sevenZip, "a", "-tzip", "-mx7", "-mm=Deflate64", os.path.join(dir_path, archiveName) + ".zip", build_path], stderr=subprocess.STDOUT)
 else:
