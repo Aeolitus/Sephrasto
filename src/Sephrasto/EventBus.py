37f4e9dada0da34896e9501e2569139aee98c880
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2023-11-29 09:27:00 +0100

    prevent plugins from loading if they use the old serialization actions/filters to prevent unnoticed dataloss

--- src/Sephrasto/EventBus.py
+++ src/Sephrasto/EventBus.py
@@ -11,9 +11,13 @@ class PriotizedCallback():
 class EventBus:
     actionCallbacks = {}
     filterCallbacks = {}
+    _deprecated = { "charakter_xml_laden", "charakter_xml_geladen", "charakter_xml_schreiben", "datenbank_xml_laden", "datenbank_xml_schreiben" }
 
     @staticmethod
     def addFilter(filterName, callback, priority=0):
+        if filterName in EventBus._deprecated:
+            raise Exception(f"Der Filter {filterName} wird nicht mehr unterstützt.")
+
         if not (filterName in EventBus.filterCallbacks):
             EventBus.filterCallbacks[filterName] = []
         cb = PriotizedCallback()
@@ -33,6 +37,9 @@ class EventBus:
 
     @staticmethod
     def addAction(actionName, callback, priority=0):
+        if actionName in EventBus._deprecated:
+            raise Exception(f"Die Action {actionName} wird nicht mehr unterstützt.")
+
         if not (actionName in EventBus.actionCallbacks):
             EventBus.actionCallbacks[actionName] = []
         cb = PriotizedCallback()
