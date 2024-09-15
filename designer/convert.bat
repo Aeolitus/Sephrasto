6600a932af4d12a0f343abea56bcf8f5b77ac9b4
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2024-09-15 14:14:36 +0200

    Update designer batch script to use venv

--- designer/convert.bat
+++ designer/convert.bat
@@ -3,8 +3,8 @@
 cd /d %0\..
 
 for /r %%f in (*.ui) do (
-    ECHO pyside6-uic.exe %%~nf.ui -o ../src/Sephrasto/UI/%%~nf.py
-    CALL pyside6-uic.exe %%~nf.ui -o ../src/Sephrasto/UI/%%~nf.py
+    ECHO ..\venv\Scripts\pyside6-uic.exe %%~nf.ui -o ../src/Sephrasto/UI/%%~nf.py
+    CALL ..\venv\Scripts\pyside6-uic.exe %%~nf.ui -o ../src/Sephrasto/UI/%%~nf.py
 )
 
 PAUSE
